9e611d14d018e087e74496131cf6f868bfe278d4
Rob Lohmann <rob@ecare.nl>
2021-04-23 15:14:05 +0200

    initial commit

---
+++ src/CodeCube.Core.Extensions.Drawing/ByteArrayExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+
+namespace CodeCube.Core.Extensions.Drawing
+{
+    /// <summary>
+    /// Class with extension methods for bytearrays.
+    /// </summary>
+    public static class ByteArrayExtensions
+    {
+        /// <summary>
+        /// Changes the DPI value of an image to 72 DPI
+        /// </summary>
+        /// <param name="imageToFit">The image to modify.</param>
+        /// <param name="mimeType">The mimetype of the image.</param>
+        /// <param name="newSize">The desired size of the image</param>
+        /// <param name="dpi">The DPI's to set.</param>
+        /// <returns>The memorystream with the resize image as byte array.</returns>
+        public static byte[] SetDpi(this byte[] imageToFit, string mimeType, Size newSize, int dpi = 72)
+        {
+            using (MemoryStream memoryStream = new MemoryStream(), newMemoryStream = new MemoryStream())
+            {
+                memoryStream.Write(imageToFit, 0, imageToFit.Length);
+                var originalImage = new Bitmap(memoryStream);
+                var newBitmap = new Bitmap(newSize.Width, newSize.Height);
+
+                using (var canvas = Graphics.FromImage(originalImage))
+                {
+                    canvas.SmoothingMode = SmoothingMode.AntiAlias;
+                    canvas.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    canvas.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                    canvas.DrawImage(originalImage, 0, 0, newSize.Width, newSize.Height);
+
+                    newBitmap.SetResolution(dpi, dpi);
+                    newBitmap.Save(newMemoryStream, ImageFunctions.GetEncoderInfo(mimeType), null);
+                }
+                return newMemoryStream.ToArray();
+            }
+        }
+    }
+}
---
+++ src/CodeCube.Core.Extensions.Drawing/StreamExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace CodeCube.Core.Extensions.Drawing
+{
+    public static class StreamExtensions
+    {
+        /// <summary>
+        /// Get the image dimensions from the provided stream.
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static Size GetImageDimensions(this Stream stream)
+        {
+            try
+            {
+                using (var myImage = Image.FromStream(stream))
+                {
+                    return new Size(myImage.Width, myImage.Height);
+                }
+            }
+            catch(Exception exception)
+            {
+                throw new InvalidOperationException("Error getting the dimensions from the image!",exception);
+            }
+        }
+    }
+}
